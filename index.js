91127ac6a71502ffeed12577c0c32b4d13160326
Lakshay Thakur <luckylaksh737@gmail.com>
2026-04-22 17:19:40 +0530

    Project Day Four

--- index.js
+++ index.js
@@ -53,3 +53,13 @@ const swiperHome = new Swiper('.swiper', {
     disableOnInteraction: false,
   },
 });
+
+/*=============== CHANGE HEADER STYLES ===============*/
+const scrollHeader = () => 
+{
+    const header = document.getElementById('header');
+
+    this.scrollY >= 50 ? header.classList.add('scroll-header'):header.classList.remove('scroll-header');
+}
+
+window.addEventListener('scroll', scrollHeader);
